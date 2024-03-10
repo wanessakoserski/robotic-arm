05fb6303c87eb03698bc24fde0752ce0978056fb
Wanessa Koserski <wkoserski@gmail.com>
2024-03-10 18:02:30 -0300

    feat: implement movemento to left and right

--- robotic_arm.py
+++ robotic_arm.py
@@ -6,12 +6,17 @@ from basic.node import Node
 
 class RoboticArm:
     def __init__(self, number_of_positions=8, number_of_boxes=6):
+        # robotic arm information
         self.initial_arm_position = 4
-        self.space_arm_info = 2
+        self.space_arm_info = 3
+        self.arm_current_position = 0
+        self.arm_previous_position = 1
+        self.arm_picked_box = 2
+        # self.arm_last_movements = 3
+
         self.boxes_per_position = 3
         self.number_of_boxes = number_of_boxes
         self.number_of_positons = number_of_positions
-
         self.list_size = (number_of_positions * self.boxes_per_position) + self.space_arm_info
 
         self.boxes = self.generate_random_boxes(number_of_boxes)
@@ -33,7 +38,7 @@ class RoboticArm:
         self.list_positions = np.full(self.list_size, 0)
 
         # insert standard information about the arm 
-        self.list_positions[0] = self.initial_arm_position
+        self.list_positions[self.arm_current_position] = self.initial_arm_position
 
         # insert boxes
         for i in range(len(self.boxes_positions)):
@@ -45,10 +50,10 @@ class RoboticArm:
     
 
     def test_goal(self, node):
-        expected_result = self.number_of_boxes / self.boxes_per_position
+        expected_result = int(self.number_of_boxes / self.boxes_per_position)
         current_result = 0
 
-        for i in range(self.number_of_positons):
+        for i in range(expected_result):
             index = (i * self.boxes_per_position) + self.space_arm_info
 
             current_positions = []
@@ -81,10 +86,72 @@ class RoboticArm:
         final_state = ""
         for i in range(self.space_arm_info, self.list_size):
             final_state += str(state[i]) + " " 
-            if ((i - 1) % 3 == 0):
+            if ((i - (self.space_arm_info - 1)) % 3 == 0):
                 final_state += "\n"
 
         return final_state
 
 
+    def _left(self, node):
+        next_states = []
+
+        current_state = node.state
+        position = current_state[self.arm_current_position]
+        if (current_state[self.arm_previous_position] == 0): position += 1 # if it is the first movement it can get the box below the arm
+
+        for next_position in range(1, position):
+            distance = position - next_position
+            next_state = self.relocate_boxes(next_position, current_state)
+            if (next_state is not None):
+                next_states.append(Node(next_state, current_state, ("⬅" * distance)))
+
+        return next_states
     
+
+    def _right(self, node):
+        next_states = []
+
+        current_state = node.state
+        position = current_state[self.arm_current_position]
+
+        for next_position in range(position, (self.number_of_positons + 1)):
+            distance = next_position - position
+            next_state = self.relocate_boxes(next_position, current_state)
+            if (next_state is not None):
+                next_states.append(Node(next_state, current_state, ("⬅" * distance)))
+
+        return next_states
+            
+            
+    def relocate_boxes(self, position, state):
+        new_state = np.copy(state)
+        index = (position * self.boxes_per_position) + self.space_arm_info - 1 
+        if (position == 7):
+            print(">> " + str(index))
+
+        if (new_state[self.arm_picked_box] != 0):
+            next_stack_space = 0
+            while (next_stack_space != self.boxes_per_position):
+                if (new_state[index - next_stack_space] == 0):
+                    new_state[index - next_stack_space] = new_state[self.arm_picked_box]
+                    new_state[self.arm_picked_box] = 0
+                    next_stack_space == self.boxes_per_position
+
+                    return new_state
+                else:
+                    next_stack_space += 1
+        else:
+            next_stack_space = 0
+            while (next_stack_space != self.boxes_per_position):
+                if (new_state[index - next_stack_space] != 0):
+                    new_state[self.arm_picked_box] = new_state[index - next_stack_space]
+                    new_state[index - next_stack_space] = 0
+                    next_stack_space == self.boxes_per_position
+
+                    return new_state
+                else:
+                    next_stack_space += 1
+        
+        return None
+            
+            
